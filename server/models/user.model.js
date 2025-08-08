0f0233700a91fc4a4276d4a83daa985f0fe881d9
Siraj_Tariq <sirajtariq0@gmail.com>
2025-08-08 16:07:14 +0500

    Enhance user model: add password hashing and comparison methods; implement JWT token generation utility

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -36,8 +36,25 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   }
+},{timestamps: true});
+
+userSchema.pre('save', async function(next) {
+
+  if (!this.isModified('password')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+  
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.methods.matchPassword = async function (enteredPassword){
+  return await bcrypt.compare(enteredPassword, this.password)
+};
 
+const User = mongoose.model("User", userSchema);
 export default User;
